3f147f3535e0cd127b878c6ff166f0a6a06630e7
Juan Carlos Ramirez <juancarlosramirezguerrero@gmail.com>
2022-10-26 09:43:33 +0200

    funcionalidad terminada, a espera de adaptabilidad en moviles

--- juego.js
+++ juego.js
@@ -1,49 +1,48 @@
-function getRandomInt(max) {
-    return Math.round(Math.random() * max);
-}
+function randomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
 //Aleatorizamos los colores
-function getColors(){
-    let r = getRandomInt(255);
-    let g = getRandomInt(255);
-    let b = getRandomInt(255);
+function getColors() {
+    let r = randomInt(256);
+    let g = randomInt(256);
+    let b = randomInt(256);
     let rgb = [r, g, b];
-return rgb;
+    return rgb;
 }
 
-function setColors(){
+function setColors() {
     let colorArray = [];
     divs = document.getElementsByClassName('color')
-    for(i=0; i<divs.lenght; i++){
+    for (i = 0; i < divs.length; i++) {
         color = getColors();
-        divs[i].style.opacity='100%';
-        divs[i].style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})` ;
-        console.log(divs[i].style.backgroundColor)
+        divs[i].style.opacity = '100%';
+        divs[i].style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+        colorArray.push(color);
     }
-    let randomIndex = Math.floor(Math.random() * colorArray.lenght);
+    let randomIndex = randomInt(colorArray.length);
     let randomColor = colorArray[randomIndex];
-    /*document.getElementById('codigo').innerHTML = `RGB(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]})`;
+    document.getElementById('codigo').innerHTML = `RGB(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]})`;
 
     // Comprobamos que el valor del atributo style del div seleccionado es igual al valor rgb que hemos guardado
     document.getElementById("resultado").innerHTML = "Esperando a que seleccione color";
-    for(i=0; i<divs.lenght;i++){
+    for (i = 0; i < divs.length; i++) {
         divs[i].addEventListener("click", (e) => {
-            if (e.target.style.backgroundColor == `rgb(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]})`)
-              document.getElementById("resultado").innerHTML = "Enhorabueno, has ganado";
-            else
-            {
+            if (e.target.style.backgroundColor == `rgb(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]})`){
+                document.getElementById("resultado").innerHTML = "Enhorabueno, has ganado";
+                e.target.style.opacity = '100%';
+        }
+            else {
                 e.target.style.opacity = '0%';
-                e.target.style.transition= "0.5s";
+                e.target.style.transition = "0.5s";
                 document.getElementById("resultado").innerHTML = "Sigue intentando";
             }
         })
-    }*/
+    }
 }
 
-window.onload = ()=>{
-  // Generamos los nuevos colores sin tener que recargar la pagina
-
-  let redo = document.getElementsByTagName('h4');
-  redo[0].addEventListener("click", setColors);
-
-  setColors();
+window.onload = () => {
+    document.getElementsByTagName('h4')[0].addEventListener("click", setColors);
+    setColors();
 };
+
